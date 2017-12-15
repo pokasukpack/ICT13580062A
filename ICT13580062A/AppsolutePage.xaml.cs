4244eb02b0aa37fb9f9e43e05af0153315b5a25c
dyta <18145142+dyta@users.noreply.github.com>
2017-12-15 16:16:48 +0700

    รักนะ

---
+++ ICT13580062A/AppsolutePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580062A
+{
+    public partial class AppsolutePage : ContentPage
+    {
+        public AppsolutePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ICT13580062A/ICT13580062APage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580062A
+{
+    public partial class ICT13580062APage : ContentPage
+    {
+        public ICT13580062APage()
+        {
+            InitializeComponent();
+            okButton.Clicked += OkButton_Clicked;
+            genderPicker.Items.Add("ชาย");
+            genderPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged;
+            myStepper.ValueChanged += MyStepper_ValueChanged;
+        }
+
+        void OkButton_Clicked(object sender, System.EventArgs e)
+        {
+            CamelCaseLabel.Text = CamelCaseLabel2.Text;
+        }
+
+        void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        void MyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+    }
+}
